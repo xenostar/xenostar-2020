3cc5fdf7de39c51a69b952c69ca8ff886c1a6eb5
xenostar <bryan.a.leewood@gmail.com>
2020-02-04 11:26:25 -0500

    Media helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,29 @@
+import { css } from 'styled-components'
+
 export const getRandomInt = (min, max) => {
   min = Math.ceil(min)
   max = Math.floor(max)
   //The maximum is exclusive and the minimum is inclusive
   return Math.floor(Math.random() * (max - min)) + min
 }
+
+const sizes = {
+  desktop: 992,
+  tablet: 768,
+  phone: 376
+}
+
+/**
+ * Iterate through the sizes and create a media template
+ */
+export const media = Object.keys(sizes).reduce((accumulator, label) => {
+  // use em in breakpoints to work properly cross-browser and support users
+  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+  const emSize = sizes[label] / 16
+  accumulator[label] = (...args) => css`
+    @media (max-width: ${emSize}em) {
+      ${css(...args)}
+    }
+  `
+  return accumulator
+}, {})

b06df409e538baea3bb1f520a5752401055cfce8
xenostar <bryan.a.leewood@gmail.com>
2020-02-26 01:07:37 -0500

    Cleaning up gatsby-node createPage

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,69 +1,40 @@
 const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.createPages = ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allDatoCmsBlogPost(sort: {fields: publishDate, order: DESC}) {
-          nodes {
+  const result = await graphql(`
+    {
+      allDatoCmsBlogPost(sort: {fields: publishDate, order: DESC}) {
+        nodes {
+          title
+          slug
+          publishDate(formatString: "MMMM Do, YYYY", locale: "en")
+          body
+          excerpt
+          tags
+          categories
+          seo {
             title
-            slug
-            publishDate(formatString: "MMMM Do, YYYY", locale: "en")
-            body
-            excerpt
-            tags
-            categories
-            seo {
-              title
-              description
-            }
+            description
           }
         }
       }
-    `).then(result => {
-      result.data.allDatoCmsBlogPost.nodes.map(data => {
-        createPage({
-          path: `blog/${data.slug}`,
-          component: path.resolve(`./src/templates/Post.js`),
-          context: {
-            slug: data.slug,
-            data: data,
-          },
-        })
-      })
-      resolve()
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  result.data.allDatoCmsBlogPost.nodes.map(data => {
+    createPage({
+      path: `blog/${data.slug}`,
+      component: path.resolve(`./src/templates/Post.js`),
+      context: {
+        data: data,
+      },
     })
   })
 }
-
-// exports.createPages = ({ graphql, actions }) => {
-//   const { createPage } = actions
-
-//   return new Promise((resolve, reject) => {
-//     graphql(`
-//       {
-//         allDatoCmsWork {
-//           edges {
-//             node {
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     `).then(result => {
-//       result.data.allDatoCmsWork.edges.map(({ node: work }) => {
-//         createPage({
-//           path: `works/${work.slug}`,
-//           component: path.resolve(`./src/templates/work.js`),
-//           context: {
-//             slug: work.slug,
-//           },
-//         })
-//       })
-//       resolve()
-//     })
-//   })
-// }

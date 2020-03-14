03f6b9ff7afae18d3180a8000570e925320af03d
xenostar <bryan.a.leewood@gmail.com>
2020-03-13 23:36:52 -0400

    Removing unused components

--- src/components/Headers/HeaderProject.js
+++ src/components/Headers/HeaderProject.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { Row, Col as Col_, BlogDate, BlogShare as BlogShare_ } from 'components'
+import { Row, Col as Col_ } from 'components'
 import { media } from 'utils'
 
 export const HeaderProject = ({
@@ -34,17 +34,4 @@ const Name = styled.h2`
   ${media.tablet`
     font-size: 10.5vw;
   `}
-`
-const Meta = styled.div`
-  display: flex;
-  margin-bottom: ${props => props.theme.layout.spacing};
-  width: 100%;
-`
-const BlogShare = styled(BlogShare_)`
-  border-left: 1px solid #ddd;
-  margin-left: ${props => props.theme.layout.spacingSmall};
-  padding-left: ${props => props.theme.layout.spacingSmall};
-`
-const IntroText = styled.h5`
-  color: ${props => props.theme.colors.primary};
 `

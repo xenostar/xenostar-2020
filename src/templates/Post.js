0534017c8dac70d8deccf2acca1cff98a507bc1c
xenostar <bryan.a.leewood@gmail.com>
2020-06-08 14:03:42 -0700

    [master] Testme

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -25,6 +25,7 @@ const Post = ({ location: { pathname }, data: { datoCmsBlogPost: data } }) => {
     <Page>
       <SEO title={data.seo.title} description={data.seo.description} />
       <HeaderPost
+        test={test}
         date={data.publishDate}
         introText={data.excerpt}
         pathName={pathname}

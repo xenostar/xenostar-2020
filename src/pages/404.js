215795607a3558d0ded78f6fb91e14c8a5b4de71
xenostar <bryan.a.leewood@gmail.com>
2020-12-28 21:35:47 -0800

    #master Better 404 page. Redirect for LinkedIn.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import { Page, SEO, Section, Row, Footer } from 'components'
+import { Page, Header, SEO, Section, Footer } from 'components'
 
 const FourZeroFour = () => (
   <Page>
-    <SEO title="404" />
-    <Section>
-      <Row>
-        <p>Page not found!</p>
-      </Row>
-    </Section>
+    <SEO title="404" description="Page not found." />
+    <Header title="Oops!" introText="This page appears to be missing..." />
+    <Section />
     <Footer />
   </Page>
 )

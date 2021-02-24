4f11af47b42a3e8123bebebc9ff56665e4c4fa01
xenostar <bryan.a.leewood@gmail.com>
2021-02-24 10:59:09 -0800

    master Adding line highlighting.

--- src/components/Content.js
+++ src/components/Content.js
@@ -76,4 +76,13 @@ export const Content = styled.div`
     border-radius: ${props => props.theme.layout.borderRadius};
     padding: ${props => props.theme.spacing.default};
   }
+  .gatsby-highlight-code-line {
+    background-color: rgba(97, 175, 239, 0.2);
+    display: block;
+    margin-right: -${props => props.theme.spacing.default};
+    margin-left: -${props => props.theme.spacing.default};
+    padding-right: ${props => props.theme.spacing.default};
+    padding-left: 1.5625rem;
+    border-left: 0.3125rem solid rgba(97, 175, 239, 0.5);
+  }
 `

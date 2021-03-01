cb30e1cd5f32c94d4abff41724cf9bcfad11da33
xenostar <bryan.a.leewood@gmail.com>
2021-03-01 09:20:37 -0800

    master Updating eslintrj.js file to follow Gatsby guidelines.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,6 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
+  globals: {
+    __PATH_PREFIX__: true
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 12, // 2021
-    sourceType: 'module'
-  },
-  rules: {
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off'
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  }
+  extends: ['react-app', 'plugin:prettier/recommended']
 }

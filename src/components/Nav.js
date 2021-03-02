821c2b2f040ee54a5d44a9645627e364282141ab
xenostar <bryan.a.leewood@gmail.com>
2021-03-02 11:33:00 -0800

    master Testing out fixed Link.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components'
 import { NavLink } from 'components'
 import { media, routes } from 'utils'
+import { Link } from 'gatsby'
 
 export const Nav = () => (
   <StyledNav>
-    <NavLink to={routes.home.url}>{routes.home.name}</NavLink>
+    <StyledTest activeClassName="active" to={routes.home.url}>
+      {routes.home.name}
+    </StyledTest>
     <NavLink to={routes.portfolio.url} partiallyActive={true}>
       {routes.portfolio.name}
     </NavLink>
@@ -14,6 +17,20 @@ export const Nav = () => (
     </NavLink>
   </StyledNav>
 )
+
+const StyledTest = styled(Link)`
+  align-items: center;
+  color: ${props => props.theme.colors.secondary};
+  display: flex;
+  font-family: ${props => props.theme.fonts.montserrat};
+  font-weight: ${props => props.theme.fonts.bold};
+  padding: 0 15px;
+  transition: ${props => props.theme.transitions.default};
+  &.active,
+  :hover {
+    color: ${props => props.theme.colors.primary};
+  }
+`
 
 const StyledNav = styled.nav`
   display: flex;

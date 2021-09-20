f8616b1cdfa6aaf8a465a5d4f378a4c66aadeff0
xenostar <bryan.a.leewood@gmail.com>
2021-09-20 10:29:14 -0700

    master Setting up different themes.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,14 +11,14 @@ import {
   TopBar,
   MobileNav
 } from 'components'
-import { store, theme } from 'utils'
+import { store, lightTheme } from 'utils'
 import 'normalize.css'
 
 const Layout = ({ children, path }) => {
   // const [isDarkMode, setIsDarkMode] = useState(false)
   return (
     <StoreProvider store={store}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={lightTheme}>
         <HelmetProvider>
           <StoreSync path={path} />
           <SEO />
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -40,40 +40,6 @@ export const theme = {
     maxWidthFixedUltrawide: '76em', // 1216px
     topBar: '6px'
   },
-  palette: {
-    // primary: '#90c7a8',
-    // gradient: 'linear-gradient(to right, #ec56f4, #90c7a8)',
-    // primary: '#90c7a8',
-    // gradient: 'linear-gradient(to right, #49a19a, #90c7a8)',
-    // primary: '#90c7a8',
-    // gradient: 'linear-gradient(to right, #379189, #90c7a8)',
-    // primary: '#90c7a8',
-    // gradient: 'linear-gradient(to right, #8764b2, #90c7a8)',
-    // primary: '#90c7a8',
-    // gradient: 'linear-gradient(to right, #6f43a4, #90c7a8)',
-    // primary: '#00cdac',
-    // gradient: 'linear-gradient(to right, #02aab0, #00cdac)',
-    // primary: '#43cea2',
-    // gradient: 'linear-gradient(to right, #185a9d, #43cea2)',
-    // primary: '#5b86e5',
-    // gradient: 'linear-gradient(to right, #36d1dc, #5b86e5)',
-    background: {
-      default: 'rgba(255, 255, 255, 1)', // #fff
-      oneDarkPro: 'rgba(40, 44, 52, 1)' // #282c34
-    },
-    common: {
-      black: 'rgba(0, 0, 0, 1)', // #000
-      white: 'rgba(255, 255, 255, 1)', // #fff
-      transparent: 'rgba(0, 0, 0, 0)' // Transparent
-    },
-    primary: '#90c7a8',
-    secondary: 'rgba(34, 34, 34, 1)', // #222
-    tertiary: 'rgba(35, 33, 33, 1)', // #232121
-    gradient: 'linear-gradient(to right, #90c7a8, #c7e3d3)',
-    lightGreen: 'rgba(199, 227, 211, 1)', // #c7e3d3
-    lightGrey: 'rgba(153, 153, 153, 1)', // #999
-    boxShadow: '0 10px 20px -10px rgba(0, 0, 0, 1)'
-  },
   shape: {
     borderRadius: {
       small: '1px',
@@ -115,5 +81,49 @@ export const theme = {
     modal: 1300,
     snackbar: 1400,
     tooltip: 1500
+  }
+}
+
+export const darkTheme = {
+  ...theme,
+  palette: {
+    background: {
+      default: 'rgba(255, 255, 255, 1)', // #fff
+      oneDarkPro: 'rgba(40, 44, 52, 1)' // #282c34
+    },
+    common: {
+      black: 'rgba(0, 0, 0, 1)', // #000
+      white: 'rgba(255, 255, 255, 1)', // #fff
+      transparent: 'rgba(0, 0, 0, 0)' // Transparent
+    },
+    primary: '#90c7a8',
+    secondary: 'rgba(34, 34, 34, 1)', // #222
+    tertiary: 'rgba(35, 33, 33, 1)', // #232121
+    gradient: 'linear-gradient(to right, #90c7a8, #c7e3d3)',
+    lightGreen: 'rgba(199, 227, 211, 1)', // #c7e3d3
+    lightGrey: 'rgba(153, 153, 153, 1)', // #999
+    boxShadow: '0 10px 20px -10px rgba(0, 0, 0, 1)'
+  }
+}
+
+export const lightTheme = {
+  ...theme,
+  palette: {
+    background: {
+      default: 'rgba(255, 255, 255, 1)', // #fff
+      oneDarkPro: 'rgba(40, 44, 52, 1)' // #282c34
+    },
+    common: {
+      black: 'rgba(0, 0, 0, 1)', // #000
+      white: 'rgba(255, 255, 255, 1)', // #fff
+      transparent: 'rgba(0, 0, 0, 0)' // Transparent
+    },
+    primary: '#90c7a8',
+    secondary: 'rgba(34, 34, 34, 1)', // #222
+    tertiary: 'rgba(35, 33, 33, 1)', // #232121
+    gradient: 'linear-gradient(to right, #90c7a8, #c7e3d3)',
+    lightGreen: 'rgba(199, 227, 211, 1)', // #c7e3d3
+    lightGrey: 'rgba(153, 153, 153, 1)', // #999
+    boxShadow: '0 10px 20px -10px rgba(0, 0, 0, 1)'
   }
 }
